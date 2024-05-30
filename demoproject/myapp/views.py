8f3f9da275eeac310ebeab318dc6ab92c642084d
surajjayraman <suraj.jayraman@gmail.com>
2024-05-30 12:07:05 -0400

    Update demoproject urlpatterns

--- demoproject/myapp/views.py
+++ demoproject/myapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the myapp index.")
+def home(request):
+    return HttpResponse("Hello, world. You're at the myapp home page.")
