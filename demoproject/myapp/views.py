5d7b5b908009d6df68a3ae92b0f8dbdb72653451
surajjayraman <suraj.jayraman@gmail.com>
2024-05-31 17:30:53 -0400

    Add display_datetime view function

--- demoproject/myapp/views.py
+++ demoproject/myapp/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from datetime import datetime
 
 def home(request):
     content = "<html><body><h1>Welcome to Little Lemon</h1></body></html>"
     # return HttpResponse("Hello, world. You're at the myapp home page.")
+    return HttpResponse(content)
+
+def display_datetime(request):
+    now = datetime.now()
+    content = "<html><body><h1>Current date and time: " + str(now) + "</h1></body></html>"
     return HttpResponse(content)
