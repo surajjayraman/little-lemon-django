8cb80ce64f6415a107d1599ee7e21cce415e7d3d
surajjayraman <suraj.jayraman@gmail.com>
2024-06-01 19:09:37 -0400

    Update myapp urlpatterns

--- demoproject/myapp/urls.py
+++ demoproject/myapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('datetime/', views.display_datetime, name = 'datetime'),
     path('menu/', views.menu, name = 'menu'),
     path('template/', views.load_template, name = 'template'),
+    path('url/', views.url_path, name = 'url'),
 ]
