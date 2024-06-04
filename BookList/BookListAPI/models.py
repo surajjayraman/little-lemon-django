26fd4785c6710df364bcd809487809a366b5eb32
surajjayraman <suraj.jayraman@gmail.com>
2024-06-04 12:41:02 -0400

    Add BookListAPI app urls.py

--- BookList/BookListAPI/models.py
+++ BookList/BookListAPI/models.py
@@ -13,12 +13,3 @@ class Book(models.Model):
 
 
 
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('books',views.books),
-    # path('books/<int:pk>',views.book),
-]
-
-# Solution code for urls.py (app-level):
