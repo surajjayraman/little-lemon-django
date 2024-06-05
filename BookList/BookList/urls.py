4cddc2d98e00f46874a7f73ca9b5fcc1d0d0aa1d
surajjayraman <suraj.jayraman@gmail.com>
2024-06-05 10:36:26 -0400

    Update BookList project urlpatterns

--- BookList/BookList/urls.py
+++ BookList/BookList/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from BookListAPI import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/',views.books),
-    path('drf/',views.getDrf)
+    path('drf/',views.getDrf),
+    path('api/',include('BookListAPI.urls'))
 ]
