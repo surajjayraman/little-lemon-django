5d360825dc58ccc08c929377107e035bfd7d44de
surajjayraman <suraj.jayraman@gmail.com>
2024-06-05 12:56:24 -0400

    Update BookView with PUT support

--- BookList/BookListAPI/views.py
+++ BookList/BookListAPI/views.py
@@ -41,16 +41,17 @@ def getDrf(request):
 
 # Different types of routing examples
 class Orders():
-	@staticmethod
-	@api_view()
-	def listOrders(request):
+    @staticmethod
+    @api_view()
+    def listOrders(request):
          return Response({'message':'list of orders'}, 200)
 
 # Mapping class that extends the APIView
 class BookView(APIView):
-	def get(self, request, pk):
+    def get(self, request, pk):
          return Response({"message":"single book with id " + str(pk)}, status.HTTP_200_OK)
-    # def put(self, request, pk):
-    #      return Response({"title":request.data.get('title')}, status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        return Response({"title":request.data.get('title')}, status.HTTP_200_OK)
 
 
