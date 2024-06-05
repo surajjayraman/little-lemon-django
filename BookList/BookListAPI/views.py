061642a4b3987b5d3a06372933e7fc35f98f11ff
surajjayraman <suraj.jayraman@gmail.com>
2024-06-05 12:27:07 -0400

    Update BookList view extends APIView

--- BookList/BookListAPI/views.py
+++ BookList/BookListAPI/views.py
@@ -8,6 +8,7 @@ from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 
 # Create your views here.
