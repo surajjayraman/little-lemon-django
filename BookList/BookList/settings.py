6a0223111f9b1c76cfcafc7f1f08ed1412a481e9
surajjayraman <suraj.jayraman@gmail.com>
2024-06-06 16:28:23 -0400

    Add debug toolbar to middleware settings

--- BookList/BookList/settings.py
+++ BookList/BookList/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware', # Add this line to the list of middleware
 ]
 
 ROOT_URLCONF = 'BookList.urls'
