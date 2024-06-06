ba8eafe64ae49b7e427c210f7fe6d59d6ef4e1b3
surajjayraman <suraj.jayraman@gmail.com>
2024-06-06 14:49:37 -0400

    Update BookListAPI urlpatterns

--- BookList/BookListAPI/urls.py
+++ BookList/BookListAPI/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('books',views.books),
     path('drf',views.getDrf),
     path('orders',views.Orders.listOrders),
-    path('books/<int:pk>',views.BookView.as_view()),
+    # path('books/<int:pk>',views.BookView.as_view()),
     path('booklist',views.BookList.as_view()),
-    path('book/<int:pk>',views.Book.as_view()),
+    path('books/<int:pk>',views.Book.as_view()),
 ]
