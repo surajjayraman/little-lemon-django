070bf6efac0a1d3962c2ea5f53db45952d4d490e
surajjayraman <suraj.jayraman@gmail.com>
2024-06-07 15:32:47 -0400

    Add MenuItemsView func to app

--- LittleLemon/LittlelemonAPI/views.py
+++ LittleLemon/LittlelemonAPI/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
