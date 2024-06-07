214309b573cdd76d8cd886e5bc0d62a17e943c5e
surajjayraman <suraj.jayraman@gmail.com>
2024-06-07 16:17:19 -0400

    Add class view SingleMenuItemView

--- LittleLemon/LittlelemonAPI/views.py
+++ LittleLemon/LittlelemonAPI/views.py
@@ -8,3 +8,7 @@ from .serializers import MenuItemSerializer
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
