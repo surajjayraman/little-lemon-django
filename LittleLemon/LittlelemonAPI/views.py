34995c62a5be3b9d6154dc98ea4bae7a4decc938
surajjayraman <suraj.jayraman@gmail.com>
2024-06-08 12:51:07 -0400

    Add view func to list all menu items

--- LittleLemon/LittlelemonAPI/views.py
+++ LittleLemon/LittlelemonAPI/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 
@@ -12,3 +14,12 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+
+# view function to list all menu items
+@api_view(['GET'])
+def menu_items(request):
+    menu_items = MenuItem.objects.all()
+    serializer = MenuItemSerializer(menu_items, many=True)
+    return Response(serializer.data)
+
