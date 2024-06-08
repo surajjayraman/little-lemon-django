98b7b921eac2ae0ac15a71bb2d9bca485358e237
surajjayraman <suraj.jayraman@gmail.com>
2024-06-08 13:03:39 -0400

    Update serializers to hide fields

--- LittleLemon/LittlelemonAPI/serializers.py
+++ LittleLemon/LittlelemonAPI/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from .models import MenuItem
 
-class MenuItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MenuItem
-        fields = ['id', 'title', 'price', 'inventory']
+# class MenuItemSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MenuItem
+#         fields = ['id', 'title', 'price', 'inventory']
+
+class MenuItemSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
 
 
