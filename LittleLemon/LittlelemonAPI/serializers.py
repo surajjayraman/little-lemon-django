06f1e5fcbacc7f4b0c8696f1b206b8851bcab379
surajjayraman <suraj.jayraman@gmail.com>
2024-06-08 13:06:10 -0400

    Update serializers

--- LittleLemon/LittlelemonAPI/serializers.py
+++ LittleLemon/LittlelemonAPI/serializers.py
@@ -9,6 +9,9 @@ from .models import MenuItem
 class MenuItemSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    inventory = serializers.IntegerField()
+
 
 
 
