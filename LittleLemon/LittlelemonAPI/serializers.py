c189a2041c3d6c998631cb8205edc00a1a55908f
surajjayraman <suraj.jayraman@gmail.com>
2024-06-08 14:23:37 -0400

    Refactor MenuItemSerializer with Model serializer

--- LittleLemon/LittlelemonAPI/serializers.py
+++ LittleLemon/LittlelemonAPI/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 from .models import MenuItem
 
-# class MenuItemSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = MenuItem
-#         fields = ['id', 'title', 'price', 'inventory']
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'title', 'price', 'inventory']
 
-class MenuItemSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
-    inventory = serializers.IntegerField()
+# class MenuItemSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(max_digits=6, decimal_places=2)
+#     inventory = serializers.IntegerField()
 
 
 
