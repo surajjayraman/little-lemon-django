766ece91e9797a7c758c71fd9b78c1d1841b1732
surajjayraman <suraj.jayraman@gmail.com>
2024-06-09 18:00:27 -0400

    Update model MenuItem with fk field Category

--- LittleLemon/LittlelemonAPI/models.py
+++ LittleLemon/LittlelemonAPI/models.py
@@ -9,4 +9,5 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
 
