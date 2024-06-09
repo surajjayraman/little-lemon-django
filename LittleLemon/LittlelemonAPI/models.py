71c6f3926514591ba3aacebd06d59eb2ba67329c
surajjayraman <suraj.jayraman@gmail.com>
2024-06-09 17:11:31 -0400

    Update model Category

--- LittleLemon/LittlelemonAPI/models.py
+++ LittleLemon/LittlelemonAPI/models.py
@@ -2,12 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name
-    
-    
 class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
