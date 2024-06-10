bced0ae6a0be1c962cc6c6264d2479457c5acd99
surajjayraman <suraj.jayraman@gmail.com>
2024-06-10 08:04:59 -0400

    create a category view urls.py map

--- LittleLemon/LittlelemonAPI/urls.py
+++ LittleLemon/LittlelemonAPI/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('menuitems', views.menu_items),
     path('menuitems/<int:pk>', views.single_item),
+    path('categories/<int:pk>', views.category_detail),
 ]
