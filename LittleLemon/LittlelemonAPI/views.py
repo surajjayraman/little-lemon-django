36fdc419d146567274d315c67cf7c41e4c8dc02f
surajjayraman <suraj.jayraman@gmail.com>
2024-06-10 08:03:46 -0400

    create a new function in the views.py file that will handle the categoryId endpoints

--- LittleLemon/LittlelemonAPI/views.py
+++ LittleLemon/LittlelemonAPI/views.py
@@ -4,14 +4,29 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
 from django.shortcuts import get_object_or_404
+
 # view function to list all menu items
 @api_view()
 def menu_items(request):
     menu_items = MenuItem.objects.select_related('category').all()
     serializer = MenuItemSerializer(menu_items, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def single_item (request, pk):
+    # menu_item = MenuItem.objects.get(pk=pk)
+    menu_item = get_object_or_404(MenuItem, pk=pk)
+    serializer = MenuItemSerializer(menu_item)
+    return Response(serializer.data)
+
+# view function to list all categories
+@api_view()
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    serializer = CategorySerializer(category)
     return Response(serializer.data)
 
 class MenuItemsView(generics.ListCreateAPIView):
@@ -24,10 +39,3 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 
 
     # return Response(menu_items.values())
-
-@api_view()
-def single_item (request, pk):
-    # menu_item = MenuItem.objects.get(pk=pk)
-    menu_item = get_object_or_404(MenuItem, pk=pk)
-    serializer = MenuItemSerializer(menu_item)
-    return Response(serializer.data)
